41fe323be94f2ddb5fd73224382943b6092e7c27
vsccw <vsccw@QingChunMBP.lan>
2017-08-13 19:24:54 +0800

    Update initial project.

---
+++ CuteAttribute/CuteAttribute/CuteAttribute.h
@@ -0,0 +1,19 @@
+//
+//  CuteAttribute.h
+//  CuteAttribute
+//
+//  Created by vsccw on 2017/8/13.
+//  Copyright © 2017年 https://vsccw.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CuteAttribute.
+FOUNDATION_EXPORT double CuteAttributeVersionNumber;
+
+//! Project version string for CuteAttribute.
+FOUNDATION_EXPORT const unsigned char CuteAttributeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CuteAttribute/PublicHeader.h>
+
+
