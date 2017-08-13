de01c2d82b2663cd826cf2d69492738309f26ef1
qiuncheng <qiuncheng@gmail.com>
2017-08-13 22:32:56 +0800

    Update add example.

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import CuteAttribute
 
 class ViewController: UIViewController {
+    
+    let text = "iOS 11 sets a new standard for the world’s most advanced mobile operating system. Your apps can now become more intelligent using the power of machine learning with Core ML. You can create incredible augmented reality experiences with ARKit. And you can deliver a more unified and immersive user experience with new multitasking features, including drag and drop for iPad, the new Files app, new camera APIs, new SiriKit domains, Apple Music integration, and more. 18829214288 https://vsccw.com Apple 1 Infinite Loop Cupertino, CA 95014 408-996–1010"
 
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        textView.attributedText =  text.cute
+            .from(0)
+            .to(10)
+            .yellow
+            .matchAllPhoneNumber()
+            .rgbColor(0x880011)
+            .underline(NSUnderlineStyle.styleSingle)
+            .underlineColor(UIColor.gray)
+            .matchAllURL()
+            .red
+            .singleUnderline
+            .underlineColor(UIColor.blue)
+            .match(string: "Apple")
+            .green
+            .doubleUnderline
+            .underlineColor(UIColor.orange)
+            .attributedString
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
