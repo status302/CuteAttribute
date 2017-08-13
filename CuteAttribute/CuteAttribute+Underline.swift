a848825de32f871b6911c03867ed92787d8cbd34
qiuncheng <qiuncheng@gmail.com>
2017-08-13 20:35:59 +0800

    Update add underline convenience.

---
+++ CuteAttribute/CuteAttribute+Underline.swift
@@ -0,0 +1,51 @@
+//
+//  CuteAttribute+Underline.swift
+//  CuteAttribute
+//
+//  Created by vsccw on 2017/8/13.
+//  Copyright © 2017年 https://vsccw.com. All rights reserved.
+//
+
+import UIKit
+
+extension CuteAttribute where Base: NSMutableAttributedString {
+    var singleUnderline: CuteAttribute<Base> {
+        return underline(.styleSingle)
+    }
+    
+    var noneUnderline: CuteAttribute<Base> {
+        return underline(.styleNone)
+    }
+    
+    var ThickUnderline: CuteAttribute<Base> {
+        return underline(.styleThick)
+    }
+    
+    var doubleUnderline: CuteAttribute<Base> {
+        return underline(.styleDouble)
+    }
+    
+    var patternSolidUnderline: CuteAttribute<Base> {
+        return underline(.patternSolid)
+    }
+    
+    var patternDotUnderline: CuteAttribute<Base> {
+        return underline(.patternDot)
+    }
+    
+    var patternDashUnderline: CuteAttribute<Base> {
+        return underline(.patternDash)
+    }
+    
+    var patternDashDotUnderline: CuteAttribute<Base> {
+        return underline(.patternDashDot)
+    }
+    
+    var patternDashDotDotUnderline: CuteAttribute<Base> {
+        return underline(.patternDashDotDot)
+    }
+    
+    var byWordUnderline: CuteAttribute<Base> {
+        return underline(.byWord)
+    }
+}
